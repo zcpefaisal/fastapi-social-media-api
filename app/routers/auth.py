a35d5ff39930f220868ce95e3403d5395113484a
zcpefaisal <inboxlions@gmail.com>
2025-09-26 10:34:51 +0600

    Generate bearer access token using JWT authantication

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 
 router = APIRouter(tags=['Authantication'])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    # OAuth2PasswordRequestForm will work on username field not the email field
+    # Also note that now request with raw-> json will not work, need to pass from-data 
+    # {
+    #     "username": "test3@faisalalam.me",
+    #     "password": "123456"
+    # }
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credential")
@@ -16,7 +23,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credential")
     
     # create a token
-
+    access_token = oauth2.create_access_token(data = {"user": user.id, "email": user.email})
     # return token
-    
-    return {"token": "Valid Token"}
+    return {"access_token": access_token, "token_type": "bearer"}
